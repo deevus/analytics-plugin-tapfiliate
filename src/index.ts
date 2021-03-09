edf00a6d2a59da0aa73796afe44c73e69b5d6596
Simon Hartcher <simon@simonhartcher.com>
2021-03-09 17:26:45 +1100

    lint

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
-import { AnalyticsPlugin, AnalyticsInstance } from 'analytics';
+import { AnalyticsPlugin, AnalyticsInstance } from "analytics";
 
 export function scriptLoaded(scriptSrc: string): boolean {
-  const scripts = document.querySelectorAll<HTMLScriptElement>('script[src]');
+  const scripts = document.querySelectorAll<HTMLScriptElement>("script[src]");
 
   return Array.from(scripts).some((script) => script.src === scriptSrc);
 }
 
 export function insertScript(scriptSrc: string): void {
-  const scriptElement = document.createElement('script');
+  const scriptElement = document.createElement("script");
   scriptElement.src = scriptSrc;
   scriptElement.async = true;
-  scriptElement.type = 'text/javascript';
+  scriptElement.type = "text/javascript";
 
   document.head.appendChild(scriptElement);
 }
@@ -25,19 +25,36 @@ export function insertScriptIfNotPresent(scriptSrc: string): void {
 type Metadata = Record<string, any>;
 
 interface TapStatic {
-  (action: 'create', tapfiliateId: string, options: Record<string, string>): void;
-  (action: 'detect', options?: {
-    cookie_domain?: string;
-    referral_code_param?: string;
-  }): void;
-  (action: 'customer' | 'trial' | 'lead', userId: string, options?: {
-    meta_data?: Metadata;
-  }): void;
-  (action: 'conversion', externalId?: string, amount?: number, options?: {
-    meta_data?: Metadata;
-    customer_id?: string;
-    currency?: string;
-  }, commissionType?: string): void;
+  (
+    action: "create",
+    tapfiliateId: string,
+    options: Record<string, string>
+  ): void;
+  (
+    action: "detect",
+    options?: {
+      cookie_domain?: string;
+      referral_code_param?: string;
+    }
+  ): void;
+  (
+    action: "customer" | "trial" | "lead",
+    userId: string,
+    options?: {
+      meta_data?: Metadata;
+    }
+  ): void;
+  (
+    action: "conversion",
+    externalId?: string,
+    amount?: number,
+    options?: {
+      meta_data?: Metadata;
+      customer_id?: string;
+      currency?: string;
+    },
+    commissionType?: string
+  ): void;
   loaded?: boolean;
   q?: IArguments[];
 }
@@ -51,7 +68,7 @@ declare global {
 
 export interface TapfiliatePluginConfig {
   tapfiliateId?: string;
-  customerType?: 'customer' | 'trial' | 'lead';
+  customerType?: "customer" | "trial" | "lead";
   cookieDomain?: string;
   referralCodeParam?: string;
 }
@@ -66,7 +83,7 @@ interface Params {
 
 const tapfiliatePlugin = (config: TapfiliatePluginConfig): AnalyticsPlugin => {
   const sharedConfig = {
-    name: 'tapfiliate',
+    name: "tapfiliate",
     config,
   };
 
@@ -75,30 +92,34 @@ const tapfiliatePlugin = (config: TapfiliatePluginConfig): AnalyticsPlugin => {
       ...sharedConfig,
 
       initialize({ config }: { config: TapfiliatePluginConfig }): void {
-        if (!config.tapfiliateId) throw new Error('No Tapfiliate tapfiliateId defined');
+        if (!config.tapfiliateId)
+          throw new Error("No Tapfiliate tapfiliateId defined");
 
-        const scriptSrc = 'https://script.tapfiliate.com/tapfiliate.js';
+        const scriptSrc = "https://script.tapfiliate.com/tapfiliate.js";
 
         insertScriptIfNotPresent(scriptSrc);
 
-        (function(window: Window, tapKey: 'tap') {
-          window['TapfiliateObject'] = tapKey;
+        (function (window: Window, tapKey: "tap") {
+          window["TapfiliateObject"] = tapKey;
 
-          window[tapKey] = window[tapKey] || function () {
-            const queue = (window[tapKey] as TapStatic).q = window[tapKey]?.q || [];
+          window[tapKey] =
+            window[tapKey] ||
+            function () {
+              const queue = ((window[tapKey] as TapStatic).q =
+                window[tapKey]?.q || []);
 
-            // eslint-disable-next-line prefer-rest-params
-            queue.push(arguments);
-          }
-        })(window, 'tap');
+              // eslint-disable-next-line prefer-rest-params
+              queue.push(arguments);
+            };
+        })(window, "tap");
 
-        window.tap('create', config.tapfiliateId, {
-          integration: 'javascript',
+        window.tap("create", config.tapfiliateId, {
+          integration: "javascript",
         });
       },
 
       ready({ config }: Params) {
-        window.tap('detect', {
+        window.tap("detect", {
           cookie_domain: config.cookieDomain,
           referral_code_param: config.referralCodeParam,
         });
@@ -107,7 +128,7 @@ const tapfiliatePlugin = (config: TapfiliatePluginConfig): AnalyticsPlugin => {
       identify({ payload, config }: Params): void {
         const { userId } = payload;
 
-        window.tap(config.customerType ?? 'customer', userId, {
+        window.tap(config.customerType ?? "customer", userId, {
           meta_data: payload.traits,
         });
       },
@@ -117,10 +138,14 @@ const tapfiliatePlugin = (config: TapfiliatePluginConfig): AnalyticsPlugin => {
       },
 
       methods: {
-        conversion(this: { instance: AnalyticsInstance }, externalId?: string, amount?: number) {
+        conversion(
+          this: { instance: AnalyticsInstance },
+          externalId?: string,
+          amount?: number
+        ) {
           const { traits } = this.instance.user();
 
-          window.tap('conversion', externalId, amount, {
+          window.tap("conversion", externalId, amount, {
             meta_data: traits,
           });
         },
@@ -129,6 +154,6 @@ const tapfiliatePlugin = (config: TapfiliatePluginConfig): AnalyticsPlugin => {
   } else {
     return sharedConfig;
   }
-}
+};
 
 export default tapfiliatePlugin;
