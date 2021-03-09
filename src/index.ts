c2dd7f651f28e97d11f6f90847069b9a053ff9a3
Simon Hartcher <simon@simonhartcher.com>
2021-03-09 17:23:40 +1100

    Add eslint

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import { AnalyticsPlugin, AnalyticsInstance } from 'analytics';
 
-export function scriptLoaded(scriptSrc: string) {
+export function scriptLoaded(scriptSrc: string): boolean {
   const scripts = document.querySelectorAll<HTMLScriptElement>('script[src]');
 
   return Array.from(scripts).some((script) => script.src === scriptSrc);
 }
 
-export function insertScript(scriptSrc: string) {
+export function insertScript(scriptSrc: string): void {
   const scriptElement = document.createElement('script');
   scriptElement.src = scriptSrc;
   scriptElement.async = true;
@@ -15,12 +15,14 @@ export function insertScript(scriptSrc: string) {
   document.head.appendChild(scriptElement);
 }
 
-export function insertScriptIfNotPresent(scriptSrc: string) {
+export function insertScriptIfNotPresent(scriptSrc: string): void {
   if (!scriptLoaded(scriptSrc)) {
     insertScript(scriptSrc);
   }
 }
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type Metadata = Record<string, any>;
 
 interface TapStatic {
   (action: 'create', tapfiliateId: string, options: Record<string, string>): void;
@@ -29,10 +31,10 @@ interface TapStatic {
     referral_code_param?: string;
   }): void;
   (action: 'customer' | 'trial' | 'lead', userId: string, options?: {
-    meta_data?: Record<string, any>;
+    meta_data?: Metadata;
   }): void;
   (action: 'conversion', externalId?: string, amount?: number, options?: {
-    meta_data?: Record<string, any>;
+    meta_data?: Metadata;
     customer_id?: string;
     currency?: string;
   }, commissionType?: string): void;
@@ -57,7 +59,7 @@ export interface TapfiliatePluginConfig {
 interface Params {
   payload: {
     userId: string;
-    traits: Record<string, any>;
+    traits: Metadata;
   };
   config: TapfiliatePluginConfig;
 }
@@ -85,6 +87,7 @@ const tapfiliatePlugin = (config: TapfiliatePluginConfig): AnalyticsPlugin => {
           window[tapKey] = window[tapKey] || function () {
             const queue = (window[tapKey] as TapStatic).q = window[tapKey]?.q || [];
 
+            // eslint-disable-next-line prefer-rest-params
             queue.push(arguments);
           }
         })(window, 'tap');
